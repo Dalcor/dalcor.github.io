9cc9f7931840fd9bf968bddb3415327c3b484b76
Viktor Peredera <vitia2282@gmail.com>
2023-08-01 05:18:40 +0300

    feat: testing pushed#5

--- erc223-page-sources/src/constants/articles.ts
+++ erc223-page-sources/src/constants/articles.ts
@@ -3,7 +3,7 @@ import {IArticle} from "../types";
 export const articles: IArticle[] = [
   {
     title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
+    description: "This rticle describes how bad ERC-20 is",
     url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c",
     image: "image1.png"
   },
